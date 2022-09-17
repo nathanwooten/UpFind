f05d15ca7c34a11a750b83aa38fa34b96112a2dd
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-17 18:59:03 -0400

    Create upFind.php

---
+++ upFind.php
@@ -0,0 +1,40 @@
+<?php
+
+if ( ! function_exists( 'upFind' ) ) {
+function upFind( $directory, array $directoryContains )
+{
+
+	$is = [];
+
+	while( $directory ) {
+
+		if ( is_file( $directory ) ) {
+			$directory = dirname( $directory ) . DIRECTORY_SEPARATOR;
+		} else {
+			$directory = rtrim( $directory, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR;
+		}
+
+		foreach ( $directoryContains as $contains ) {
+			$item = $directory . $contains;
+
+			if ( is_readable( $item ) ) {
+				$is[] = $item;
+			}
+		}
+
+		if ( count( $is ) === count( $directoryContains ) ) {
+			return $directory;
+		}
+
+		$parent = dirname( $directory );
+		if ( $parent === $directory ) {
+			$directory = false;
+		} else {
+			$directory = $parent;
+		}
+	}
+
+	return $directory;
+
+}
+}
